be353e47da431b825cdcf419af324d2792cc379c
Muchammad Alief Kurnia Wijaya <aliefkw26@gmail.com>
2023-01-29 12:34:00 +0700

    first commit

---
+++ Conceptual.cs
@@ -0,0 +1,114 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulConcept
+{
+    internal class Conceptual
+    {
+        public static void AsOperator()
+        {
+            Shape shape = new();
+            Circle circle = new();
+            Shape? convertToShape = circle as Shape;
+
+            Console.WriteLine($"convertions 'circle as shape' produces {convertToShape}");
+
+            Circle? convertToCircle = shape as Circle;
+
+            if(convertToCircle == null)
+            {
+                Console.WriteLine($"convertions 'shape as Circle' produces null");
+            }
+            else
+            {
+                Console.WriteLine($"convertions 'circle as shape' produces {convertToCircle}");
+            }
+
+            
+
+        }
+        public static void isOperator()
+        {
+            Circle circle = new();
+            Rectangle rectangle = new();
+            Console.WriteLine($"Is circle inherit from shape : {circle is Shape}");
+            Console.WriteLine($"Is rectangle inherit from shape : {rectangle is Shape}");
+        }
+
+        class Shape { }
+
+        class Circle : Shape { }
+
+        class Rectangle : Shape { }
+
+        public static void PassingByValue(int x)
+        {
+            x *= 2;
+            Console.WriteLine($"Body Passing by value, X value: {x}");
+        }
+
+        public static void PassingByReference(ref int x)
+        {
+            x *= 2;
+            Console.WriteLine($"Body PassingValueByRef, Y value: {x}");
+        }
+        public static void ShowNullable()
+        {
+            //variable reference type
+            string? name = "hello";
+
+            if (name == null) Console.WriteLine("null value");
+
+            //variable value type
+            int? age = null;
+            if (age.HasValue)
+            {
+                Console.WriteLine($"age : {age}");
+            }
+            else
+            {
+                Console.WriteLine("age has no value");
+            }
+
+        }
+
+    }
+
+    internal class Employee
+    {
+
+        public const decimal baseSalary = 100_000;
+        public readonly int _id = 100;
+
+        public Employee(int id)
+        {
+            _id = id;
+        }
+
+        public int Age { get; set; } = 10;
+
+        public readonly List<int> myNum = new();
+
+        public void AddNum(int num)
+        {
+            myNum.Add(num);
+        }
+
+        public void DisplayNum()
+        {
+            foreach (var item in myNum)
+            {
+                 Console.WriteLine($"{item}");
+            }
+        }
+        public static void UpdatePromo(double price, float promo, out decimal totalPrice, out decimal totalDiscount)
+        {
+            totalPrice = (decimal)((price * (promo / 100)) + price);
+            totalDiscount = (decimal)(price - promo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using UsefulConcept;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var emp = new Employee(101);
+        Console.WriteLine($"{emp._id}");
+        emp.Age = 25;
+        decimal totalPrice, totalDiscount;
+        emp.AddNum(10);
+        emp.AddNum(11);
+        emp.AddNum(12);
+
+        emp.DisplayNum();
+        //Employee.UpdatePromo(50_000,1000,out totalPrice,out totalDiscount)  ;
+        //Console.WriteLine($"Total Price {totalPrice}");
+        //Console.WriteLine($"Total Discount {totalDiscount}");
+
+
+        //Conceptual.isOperator();
+        //Conceptual.AsOperator();
+
+        //Console.WriteLine("Hello, World!");
+        //Conceptual.ShowNullable();
+
+        //int x = 10; //immutable
+        //Conceptual.PassingByValue(x);
+        //Console.WriteLine($"Final Value X: {x}");
+
+        //int y = 10; //nonImmutable
+        //Conceptual.PassingByReference(ref y);
+        //Console.WriteLine($"Final Value Y: {y}");
+    }
+}
